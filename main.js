6e47788d0c840540e047f7fdff5aba43c1bb88dc
niloy143 <niloymahmudapu@gmail.com>
2022-09-16 22:45:25 +0600

    fixes so many freaking bugs

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const assignmentGrid = () => {
             <h2 class="text-2xl font-semibold text-center">Assignment ${i+1}</h2>
             <div class="w-full flex items-center justify-around gap-3 text-xl">
                 <label for="">Obtained:</label>
-                <input class="w-12 py-1 rounded text-center font-semibold" type="text" placeholder="0" oninput="obtainedInput(this)" value="0">
+                <input class="w-12 py-1 rounded text-center font-semibold" type="text" placeholder="0" oninput="obtainedInput(this)" value="60">
             </div>
             <div class="w-full flex items-center justify-around text-xl">
                 <label for="">Submitted in: </label>
@@ -52,12 +52,16 @@ const calculate = () => {
         if(obtained !== 0 && (obtained / submission * 100) < 50) {lessHalf = true};
     }
     
-    const answer = ((requirement - totalObtained) / (assignments - finished)).toFixed(2);
+    const gapMarks = requirement - totalObtained;
+    const restAssignments = assignments - finished;
+    const answer = (gapMarks > 0 && restAssignments !== 0) ? ((gapMarks) / (restAssignments)).toFixed(2) : gapMarks <= 0 ? 'Fulfilled' : 'Not Fulfilled';
 
-    const lessHalfMsg = '<span class="text-red-400 font-semibold"> Oops! You got less than 50% marks in minimum one assigment.</span>';
+    const lessHalfMsg = '<span class="text-red-400 font-semibold"> Oops! You got less than 50% marks in minimum one assignment.</span>';
     const overLimitMsg = `<span class="text-red-400 font-semibold">Oops! It\'s not possible to get ${answer} out of 60'.</span>`;
-    const halfOkMsg = 'you should not get less than <span class="text-yellow-200 font-semibold">50%</span> marks in any assignment.';
+    const halfOkMsg = 'you should not get less than <span class="text-yellow-200 font-semibold">50%</span> marks in any rest of assignments.';
     const limitOkMsg = `You need to get at least <span class="text-lime-400 font-semibold">${Math.ceil(answer)}</span> marks in each rest of assignments.`;
+    const fulfilledMsg = `<span class="text-green-400 font-semibold">Congratulations!</span> you fulfilled the requirements.`;
+    const notfulfilledMsg = `<span class="text-red-400 font-semibold">Sorry but true!</span> you couldn't fulfill the requirements.`;
     
     result.innerHTML = `
         <ul class="max-w-xs mx-auto">
@@ -66,8 +70,8 @@ const calculate = () => {
             <li class="flex justify-between"><span>Assignment Finished:</span><span class="font-semibold text-lime-400">${finished}</span></li>
             <li class="flex justify-between"><span>You Obtained:</span><span class="font-semibold text-lime-400">${totalObtained}</span></li>
         </ul>
-        <h3 class="text-2xl text-center text-${answer <= 60 ? 'lime' : 'red'}-400 font-semibold my-3">${answer}</h3>
-        <p class="text-center">${answer > 60 ? overLimitMsg : limitOkMsg} <br> ${lessHalf ? lessHalfMsg : answer <= 60 ? halfOkMsg : ''}</p>
+        <h3 class="text-2xl text-center text-${(answer > 60 || answer === 'Not Fulfilled') ? 'red': 'lime'}-400 font-semibold my-3">${answer}</h3>
+        <p class="text-center">${answer > 60 ? overLimitMsg : (answer === 'Fulfilled' && !lessHalf) ? fulfilledMsg : (answer === 'Fulfilled' && lessHalf) ? 'But' : answer === 'Not Fulfilled' ? notfulfilledMsg : limitOkMsg} <br> ${lessHalf ? lessHalfMsg : (answer <= 60 && answer !== 'Fulfilled' && answer !== 'Not Fulfilled' ) ? halfOkMsg : ''}</p>
         `
     }
     
